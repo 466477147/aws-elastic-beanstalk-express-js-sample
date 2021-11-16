0acd34cbd2978cd28c88a4a20ca296ae03fe82cd
Charles LV -CHINA LAB- <charles.lv.partner@decathlon.com>
2021-11-16 16:05:32 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Change!'));
+app.get('/', (req, res) => res.send('Change!，Finish deploy'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
